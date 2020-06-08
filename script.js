d91b7e51ed1b14633c00a618acbff86477fbd3d8
Matthew Fass <matthewfass@gmail.com>
2020-06-08 12:59:53 -0400

    Style and layout changes

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const productDescription = `
+<p>Our premium woven labels capture a high amount of detail and include up to 8 thread colors. High quality labels reinforce the brand identity of your high quality products, adding a unique and identifiable finishing touch.</p>
+<p>Receive a same day digital proof when you order before 12:00 PM EST Monday-Friday. Our artists will prepare your proof within 2 business days maximum depending on current volume. You will receive an email notification when it's ready. Approve your proof or request any adjustments if necessary from your customer account. Your guaranteed "Will Ship Date" is generated when you approve your proof!</p>
+<h3>Fast Turnaround Available</h3>
+`;
+
+
+
+const googleReviews = `
+
+
+
+
+
+
+
+
+
+
+
+
+
+`
